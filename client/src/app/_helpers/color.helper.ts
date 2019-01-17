34a81776834bae82dd4b2ff252e10aa572be8f84
Daniil <d.doronkin@itransition.com>
2019-01-17 19:04:50 +0300

    added colors helper

---
+++ client/src/app/_helpers/color.helper.ts
@@ -0,0 +1,18 @@
+import { BlackHex, WhiteHex } from '../constants/colors.constants';
+
+function isLightHex(hex: string): boolean {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+  const r = parseInt(result[1], 16);
+  const g = parseInt(result[2], 16);
+  const b = parseInt(result[3], 16);
+
+  const brightness = Math.round((r * 299 + g * 587 + b * 114) / 1000);
+  return brightness > 125;
+}
+
+function getTextColor(backgroundColorHex: string): string {
+  return isLightHex(backgroundColorHex) ? BlackHex : WhiteHex;
+}
+
+export { getTextColor };
---
+++ client/src/app/constants/colors.constants.ts
@@ -0,0 +1,5 @@
+export const BlackHex = "#000000";
+export const WhiteHex = "#FFFFFF";
+export const DangerHex = "#BD362F";
+export const WarningHex = "#ffbf00";
+export const SuccessHex = "#51A351";
