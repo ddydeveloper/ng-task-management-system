6e535f05bec9969da4c487c08875852c9c4e32bc
Daniil <d.doronkin@itransition.com>
2019-01-31 17:43:32 +0300

    configured deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t ddydeveloper/task-management-client:latest -t ddydeveloper/task-management-client:$SHA -f ./client/Dockerfile.stage ./client 
+docker build -t ddydeveloper/task-management-server:latest -t ddydeveloper/task-management-server:$SHA -f ./api/Dockerfile.stage    ./api 
+docker build -t ddydeveloper/task-management-mssql:latest  -t ddydeveloper/task-management-mssql:$SHA  -f ./database/Dockerfile.k8s ./database
+
+docker push ddydeveloper/task-management-client:latest
+docker push ddydeveloper/task-management-server:latest
+docker push ddydeveloper/task-management-mssql:latest
+
+docker push ddydeveloper/task-management-client:$SHA
+docker push ddydeveloper/task-management-server:$SHA
+docker push ddydeveloper/task-management-mssql:$SHA
+
+kubectl apply k8s
+kubectl set image deployments/client-deployment server=ddydeveloper/task-management-client:$SHA
+kubectl set image deployments/server-deployment server=ddydeveloper/task-management-server:$SHA
+kubectl set image deployments/mssql-deployment  server=ddydeveloper/task-management-mssql:$SHA
