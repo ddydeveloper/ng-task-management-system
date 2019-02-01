6ac994928f1f6cf7ded61a7280b63019885565e0
Daniil <d.doronkin@itransition.com>
2019-02-01 14:47:47 +0300

    add notice

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ docker push ddydeveloper/task-management-server:$SHA
 docker push ddydeveloper/task-management-mssql:$SHA
 
 kubectl apply -f k8s
+# create secrets if needed via GKE console to keep data hidden 
 kubectl set image deployments/client-deployment server=ddydeveloper/task-management-client:$SHA
 kubectl set image deployments/server-deployment server=ddydeveloper/task-management-server:$SHA
 kubectl set image deployments/mssql-deployment  server=ddydeveloper/task-management-mssql:$SHA
