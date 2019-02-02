ab9ea7e88373f328045acdbb82041414e4a34104
Daniil <ddydeveloper@gmail.com>
2019-02-02 13:36:29 +0300

    disable db updates

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t ddydeveloper/task-management-client:latest -t ddydeveloper/task-management-client:$SHA -f ./client/Dockerfile.stage ./client 
 docker build -t ddydeveloper/task-management-server:latest -t ddydeveloper/task-management-server:$SHA -f ./api/Dockerfile.stage    ./api 
-docker build -t ddydeveloper/task-management-mssql:latest  -t ddydeveloper/task-management-mssql:$SHA  -f ./database/Dockerfile.k8s ./database
+docker build -t ddydeveloper/task-management-mssql:latest -f ./database/Dockerfile.k8s ./database
 
 docker push ddydeveloper/task-management-client:latest
 docker push ddydeveloper/task-management-server:latest
@@ -8,7 +8,6 @@ docker push ddydeveloper/task-management-mssql:latest
 
 docker push ddydeveloper/task-management-client:$SHA
 docker push ddydeveloper/task-management-server:$SHA
-docker push ddydeveloper/task-management-mssql:$SHA
 
 kubectl apply -f k8s
 
@@ -18,4 +17,4 @@ kubectl create secret generic mssql-secret --from-literal SA_PASSWORD="P@ssw0rd"
 
 kubectl set image deployments/client-deployment client=ddydeveloper/task-management-client:$SHA
 kubectl set image deployments/server-deployment server=ddydeveloper/task-management-server:$SHA
-kubectl set image deployments/mssql-deployment  mssql=ddydeveloper/task-management-mssql:$SHA
+kubectl set image deployments/mssql-deployment  mssql=ddydeveloper/task-management-mssql:latest
