86b5ca813202dc489b79e8f01a191648a4ebd874
Daniil <d.doronkin@itransition.com>
2019-02-05 14:11:38 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,15 @@ docker push ddydeveloper/task-management-client:$SHA
 docker push ddydeveloper/task-management-server:$SHA
 docker push ddydeveloper/task-management-mssql:$SHA
 
-kubectl apply -f k8s
-
 # Apply command separately to keep data in secure way
 kubectl delete secret mssql-secret
 kubectl create secret generic mssql-secret --from-literal SA_PASSWORD="P@ssw0rd" --from-literal TASKS_DB="Server=mssql-cluster-ip-service;DataBase=Tasks;User Id=sa;Password=P@ssw0rd;Connection Timeout=30;"
 
+# Apply workloads and services
+kubectl apply -f k8s
 kubectl set image deployments/client-deployment client=ddydeveloper/task-management-client:$SHA
 kubectl set image deployments/server-deployment server=ddydeveloper/task-management-server:$SHA
 
 # Database initialize single instance, set up availability level to replicate
-# kubectl apply -f k8s_db
-# kubectl set image deployments/mssql-deployment  mssql=ddydeveloper/task-management-mssql:$SHA
+kubectl apply -f k8s_db
+kubectl set image deployments/mssql-deployment  mssql=ddydeveloper/task-management-mssql:$SHA
