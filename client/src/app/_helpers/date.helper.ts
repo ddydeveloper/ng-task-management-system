a502d4ccdd5150a970911cfc910834ec53c2eed2
Daniil <ddydeveloper@gmail.com>
2019-06-19 22:34:51 +0300

    deleted

--- client/src/app/_helpers/date.helper.ts
+++ client/src/app/_helpers/date.helper.ts
@@ -8,38 +8,38 @@ function getDateDiffInSeconds(start: Date, end: Date): number {
 function secondsToText(value: number): string {
     let count = 60 * 60 * 24 * 7;
     if (value > count) {
-        return `About ${Math.floor(value / count)} weeks to complete`;
+        return `${Math.floor(value / count)} weeks to complete`;
     }
 
     if (value < -count) {
-        return `About ${Math.abs(Math.floor(value / count))} weeks expired`;
+        return `${Math.abs(Math.floor(value / count))} weeks expired`;
     }
 
     count /= 7;
     if (value > count) {
-        return `About ${Math.floor(value / count)} days to complete`;
+        return `${Math.floor(value / count)} days to complete`;
     }
 
     if (value < -count) {
-        return `About ${Math.abs(Math.floor(value / count))} days expired`;
+        return `${Math.abs(Math.floor(value / count))} days expired`;
     }
 
     count /= 24;
     if (value > count) {
-        return `About ${Math.floor(value / count)} hours to complete`;
+        return `${Math.floor(value / count)} hours to complete`;
     }
 
     if (value < -count) {
-        return `About ${Math.abs(Math.floor(value / count))} hours expired`;
+        return `${Math.abs(Math.floor(value / count))} hours expired`;
     }
 
     count /= 60;
     if (value > count) {
-        return `About ${Math.floor(value / count)} minutes`;
+        return `${Math.floor(value / count)} minutes`;
     }
 
     if (value < -count) {
-        return `About ${Math.abs(Math.floor(value / count))} minutes expired`;
+        return `${Math.abs(Math.floor(value / count))} minutes expired`;
     }
 
     return value >= 0 ? `${value} seconds` : `${Math.abs(value)} seconds expired`;
